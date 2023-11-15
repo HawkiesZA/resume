42f9b6eefeeeb8af1a8950203a7c04449bc46720
Gerrit Vermeulen <gerrit.vermeulen@assetpool.com>
2023-11-15 11:00:01 +0200

    Use tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,13 @@
 <script>
 </script>
 
-    <div class="container">
-        <div class="content">
-            <div class="text">Hello! I'm Gerrit.</div>
-            <div class="text">
-                I'm a software developer with a BSc(CS) degree and over 10 years of experience. 
-                I've worked on backend (Python, NodeJS), frontend (Vue), infrastructure (GKE, AWS Lambda), mobile development (Android Native and Flutter), and embedded systems (C, C++, Lua).   
-            </div>
-            <div class="text">
-                I'm passionate about building things that make a positive impact in people's lives.
-            </div>
-        </div>
-    </div>
-
-<style>
-    .container {
-        padding: 2px 16px;
-    }
-    .content {
-        width: 100%;
-        padding: 10px;
-    }
-    @media only screen and (min-width: 768px) {
-        .content {
-            margin: auto;
-            width: 50%;
-            padding: 10px;
-        }
-    }
-    .text {
-        padding-bottom: 0.5em;
-    }
-</style>
+<div class="flex flex-col items-center justify-center">
+    <p class="mb-1 text-left w-full">Hello! I'm Gerrit.</p>
+    <p class="mb-1 text-left w-full">
+        I'm a software developer with a BSc(CS) degree and over 10 years of experience. 
+        I've worked on backend (Python, NodeJS), frontend (Vue), infrastructure (GKE, AWS Lambda), mobile development (Android Native and Flutter), and embedded systems (C, C++, Lua).   
+    </p>
+    <p class="mb-1 text-left w-full">
+        I'm passionate about building things that make a positive impact in people's lives.
+    </p>
+</div>
